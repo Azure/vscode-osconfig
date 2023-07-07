519cd90b907b1db6af52e1bc071234839fff55a8
daniellesilfa <134558482+daniellesilfa@users.noreply.github.com>
2023-07-06 17:20:01 -0700

    Created SCD object, added changes from prev PR (#14)

--- server/src/model/index.ts
+++ server/src/model/index.ts
@@ -4,5 +4,5 @@
  * ------------------------------------------------------------------------------------------ */
 
 export * from './repository';
-export * from './schema';
+export * from './mim';
 export * from './validator';
