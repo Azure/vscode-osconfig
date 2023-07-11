95aa429f3f450563ad8943128ab81457c028a08e
daniellesilfa <134558482+daniellesilfa@users.noreply.github.com>
2023-07-11 16:00:57 -0700

    Integrated API calls & User Input in extension.ts (#15)

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
+import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
 
 import {
   LanguageClient,
@@ -7,6 +8,8 @@ import {
   ServerOptions,
   TransportKind,
 } from 'vscode-languageclient/node';
+
+import { padUserInput, systemMessage, parameters } from './openai';
 
 let client: LanguageClient;
 
@@ -27,11 +30,13 @@ export function activate(context: vscode.ExtensionContext) {
   };
 
   const clientOptions: LanguageClientOptions = {
-    documentSelector: [{
-      scheme: 'file',
-      language: 'json',
-      pattern: '**/osconfig_desired*.json'
-    }],
+    documentSelector: [
+      {
+        scheme: 'file',
+        language: 'json',
+        pattern: '**/osconfig_desired*.json',
+      },
+    ],
     synchronize: {
       fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc'),
     },
@@ -45,6 +50,48 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   client.start();
+
+  const endpoint = process.env['AZURE_OPENAI_ENDPOINT2'];
+  const azureApiKey = process.env['AZURE_OPENAI_KEY2'];
+  const deploymentId = process.env['AZURE_DEPLOYMENT_ID'];
+
+  vscode.commands.registerCommand('vscode-osconfig.DCGenerator', async () => {
+    const userInput = await vscode.window.showInputBox({
+      placeHolder: 'Enter setting here',
+      prompt:
+        'Enter in desired configuration setting for DC document (Ex: Network Access)',
+    });
+
+    if (userInput === undefined) {
+      vscode.window.showErrorMessage('Not a valid input');
+    } else if (userInput === '') {
+      vscode.window.showErrorMessage(
+        'Setting configuration is required to execute this action'
+      );
+    } else {
+      vscode.window.showInformationMessage(
+        'Generating DC Doc with: ' + userInput
+      );
+      const userMessage = padUserInput(userInput);
+      const messages = [
+        { role: 'system', content: systemMessage },
+        { role: 'user', content: userMessage },
+      ];
+
+      if (azureApiKey && endpoint) {
+        const client = new OpenAIClient(
+          endpoint,
+          new AzureKeyCredential(azureApiKey)
+        );
+        //result variable will be used in future implementation
+        const result = await client.getChatCompletions(
+          deploymentId,
+          messages,
+          parameters
+        );
+      }
+    }
+  });
 }
 
 export function deactivate(): Thenable<void> | undefined {
